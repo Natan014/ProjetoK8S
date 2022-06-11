4aaf449dd7a242a5e3f17b43d0e3b2476eb2f643
natan014 <tan.martini05@gmail.com>
2022-06-10 22:32:37 -0300

    Atualizações

--- master.sh
+++ master.sh
@@ -15,5 +15,5 @@
     apt-get install -y kubeadm kubelet kubectl && \
     kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.10.127 && \
     mkdir -p /home/vagrant/.kube && cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config &&  chown vagrant:vagrant /home/vagrant/.kube/config && \
-    echo $(kubeadm token create --print-join-command)> /vagrat/join.sh && \
+    echo $(kubeadm token create --print-join-command) > /vagrant/join.sh && \
     kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
