bceab52601829db43aa9c7161b0d318c1943d8de
natan014 <tan.martini05@gmail.com>
2022-06-10 21:53:15 -0300

    Upando arquivos

---
+++ master.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+    apt-get update && \
+    apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release && \
+    swapoff -a && \
+    apt-get install -y docker.io && \
+    systemctl enable docker && systemctl start docker && \
+    apt-get update && 
+    echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list && \
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -   && \
+    apt-get update && \
+    apt-get install -y kubeadm kubelet kubectl && \
+    kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.10.127 && \
+    mkdir -p /home/vagrant/.kube && cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config &&  chown vagrant:vagrant /home/vagrant/.kube/config && \
+    echo $(kubeadm token create --print-join-command)> /vagrat/join.sh && \
+    kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
---
+++ worker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+    apt-get update && \
+    apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release && \
+    swapoff -a && \
+    apt-get install -y docker.io && \
+    systemctl enable docker && systemctl start docker && \
+    apt-get update && 
+    echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list && \
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -   && \
+    apt-get update && \
+    apt-get install -y kubeadm kubelet kubectl
